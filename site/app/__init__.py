4632596a860796376da66fd21adac62498049348
Sebastian Paulus <svpaulus1@cougars.ccis.edu>
2025-03-05 09:51:15 -0800

    added debug toolbar

--- site/app/__init__.py
+++ site/app/__init__.py
@@ -1,4 +1,10 @@
 # file: __init__.py
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'SOME RANDOM SECRET STRING'
+toolbar = DebugToolbarExtension(app)
+
 from app import routes
--- site/app/routes.py
+++ site/app/routes.py
@@ -1,7 +1,12 @@
 # file: routes.py
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    app.logger.info("entering index() ...")
+    x = 42
+    app.logger.info("x:", x)
+    app.logger.info("exiting index() ...")
+    return render_template('index.html', user='John')
