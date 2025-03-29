d920b4af1545601e70cee496b5b8e65289a26604
Sebastian Paulus <svpaulus1@cougars.ccis.edu>
2025-03-29 14:00:13 -0700

    nomsg

--- site/app/routes.py
+++ site/app/routes.py
@@ -1,5 +1,5 @@
 # file: routes.py
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from app import app
 
 @app.route('/')
@@ -7,7 +7,7 @@ from app import app
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
