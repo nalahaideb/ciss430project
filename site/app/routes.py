f36462d112582f9872467ccd56d7832e3f884f68
Sebastian Paulus <svpaulus1@cougars.ccis.edu>
2025-03-29 14:41:27 -0700

    nomsg

--- site/app/routes.py
+++ site/app/routes.py
@@ -2,6 +2,12 @@
 from flask import render_template, request, redirect, url_for, flash
 from app import app
 
+# NOTES
+# flash messages are possible, but not sure if i want to use them
+# commented out as of now as well as removed html support in base.html
+#
+# logic for login and register are placeholders to get the pages to work
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -15,7 +21,8 @@ def login():
         if username.lower() == 'test' and password == 'pass':
             return redirect(url_for('dashboard'))
         else:
-            flash('Invalid credentials', 'danger')
+            #flash('Invalid credentials', 'danger')
+            return redirect(url_for('login'))
     return render_template('login.html')
 
 @app.route('/register')
@@ -26,9 +33,10 @@ def register():
         password = request.form.get('password')
         terms = request.form.get('terms')
         if not terms:
-            flash('You must agree to the terms', 'warning')
+            #flash('You must agree to the terms', 'warning')
+            return redirect(url_for('register'))
         else:
-            flash('Account created successfully! Please log in.', 'success')
+            #flash('Account created successfully! Please log in.', 'success')
             return redirect(url_for('login'))
     return render_template('register.html')
 
