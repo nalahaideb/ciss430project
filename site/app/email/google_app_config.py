36f06876c6de0c882b493d2b879731b427bd563f
nalahaideb <nalahaideb1@cougars.ccis.edu>
2025-04-17 14:53:26 -0700

    updated email server creds

--- site/app/email/google_app_config.py
+++ site/app/email/google_app_config.py
@@ -1,4 +1,6 @@
-GMAIL = 'nalahaideb1@cougars.ccis.edu' # your gmail
+GMAIL = 'nalahaideb1@cougars.ccis.edu' #new gmail #mysqlpal@gmail.com
+
 #google app password of length 16 (see below)
-GOOGLE_APP_PASSWORD = 'khai ccvw gabn yoyv' 
+GOOGLE_APP_PASSWORD = 'khai ccvw gabn yoyv' #new pass:wjmm zspw uwgu maoq
+
 TO = 'svpaulus1@cougars.ccis.edu'#'nalahaideb1@cougars.ccis.edu'#
